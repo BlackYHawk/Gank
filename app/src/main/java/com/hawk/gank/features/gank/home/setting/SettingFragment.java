3fd1a74fb3751e12d0a19a3e7c07d572e7e04646
Hawk <leqinde@163.com>
2018-07-04 19:59:05 +0800

    update plugin to lastest

---
+++ app/src/main/java/com/hawk/gank/features/gank/home/setting/SettingFragment.java
@@ -0,0 +1,10 @@
+package com.hawk.gank.features.gank.home.setting;
+
+import com.hawk.lib.base.ui.fragment.BaseFragment;
+
+/**
+ * Created by heyong on 2018/1/1.
+ */
+
+public class SettingFragment extends BaseFragment {
+}
